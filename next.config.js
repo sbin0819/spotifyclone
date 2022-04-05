56e8bdb325e5a8f3b0798a790b8fc032e60256fe
sbin0819 <sbinha123@gmail.com>
2022-04-05 16:30:53 +0900

    wip: some changed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    runtime: 'nodejs',
-  },
 }
+
+module.exports = nextConfig
